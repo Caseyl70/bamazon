8991d5d01573e05f2e9ea17fcf8e22923bfc054e
Casey Levin <clevin8080@yahoo.com>
2019-10-25 00:00:58 -0600

    Finishing touches to the customer feature.

--- bamazonCust.js
+++ bamazonCust.js
@@ -1,7 +1,7 @@
 //Initializing database and using require.
 var inquirer = require("inquirer");
 var mysql = require("mysql");
-
+var totalPurchaseAmount = 0;
 // Connection
 var connection = mysql.createConnection({
     host: "localhost",
@@ -16,16 +16,73 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err)
         throw err;
+    displayData();
 });
 
-//Product Function. Where questions start 
+function displayData() {
+    connection.query("SELECT * FROM products", function(err, results) {
+        console.table(results);
+        question1(results);
+    })
+}
 
-//Quantity Function. Where customers pick and choose what product they want.
+//Product Function. Where questions start / starting off the questions
+function question1(res) {
+    inquirer.prompt([{
+            type: "input",
+            name: "item_id",
+            message: "What product would you like to purchase? (Use Item Id)",
+        }])
+        .then(function(ans) {
+            var userInput = ans.item_id;
+            var product = res.filter(function(row) {
+                return (row.item_id == userInput);
+            })[0];
+            // console.log(product);
+            question2(product);
+        })
+}
 
-// For loop for when actions are performed to update the database.
+function question2(res) {
+    inquirer.prompt([{
+            type: "input",
+            name: "stock_quantity",
+            message: "How much would you like to purchase?",
+        }])
+        .then(function(ans) {
+            var userInput = ans.stock_quantity;
+            var stockTotal = parseInt(res.stock_quantity) - parseInt(userInput);
+            if (stockTotal < 0) {
+                console.log("Out of Stock.")
+                displayData();
+            } else {
+                connection.query("UPDATE products SET stock_quantity = ? WHERE item_id = ? ", [stockTotal, res.item_id],
+                    function(err, results) {
+                        if (err)
+                            throw (err);
+                        totalPurchaseAmount += parseFloat(results.price);
+                        question3();
 
-//Ending shopping function (IF ELSE).
 
-//Update Function for the store to update quantity.
+                    });
+            }
+        })
+}
 
-//Out of Stock Function (IF ELSE).
+function question3() {
+    inquirer.prompt([{
+            type: "list",
+            name: "exit",
+            message: " Continue Shopping?",
+            choices: ["No", "Yes"]
+        }])
+        .then(function(ans) {
+            if (ans.exit === "No") {
+                console.log("Thank you For shopping With us! Come Back Soon.");
+
+            } else {
+                question1();
+            }
+
+        })
+}
--- schema.sql
+++ schema.sql
@@ -8,6 +8,38 @@ price FLOAT NOT NULL,
 stock_quantity INT NOT NUll,
 PRIMARY KEY(item_id)
 );
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES 
+("Football Helemet", "Sports", 180, 100), 
+("Kicking Tee", "Sports", 25, 130),
+("Chair", "Furiniture", 1000, 50), 
+("Rainbow Six Siege", "Video Games", 60, 150), 
+("MacBook Pro", "Electronics",3000.00 ,200 ),
+("Gloves", "Sports", 65, 100), 
+("Visor", "Sports", 50, 130),
+("Table", "Furiniture", 1200, 50), 
+("NHL 19", "Video Games", 60, 150), 
+("MacBook Air", "Electronics",3000.00 ,200 ),
+  ("Trackpad", "Electronics",30.00 ,2000 ),
+  ("AirPods", "Electronics",200.00 ,2000 ),
+  ("Beats By Dre", "Electronics",300.00 ,400 ),
+  ("Apple TV", "Electronics",350.00 ,400 ),
+  ("Drone", "Electronics",3000.00 ,200 ),
+  ("Basketball", "Sports",30.00 ,150 ),
+("Red Dead Redemption II", "Video Games", 60, 150), 
+("Call Of Duty Modern Warfare III", "Video Games", 60, 150), 
+("Assasins Creed Brother Hood", "Video Games", 60, 150), 
+("Watch Dogs", "Video Games", 60, 150), 
+("Crash Bandicoot", "Video Games", 60, 150), 
+("Mario Cart", "Video Games", 60, 150), 
+("Mario Party 8", "Video Games", 60, 150), 
+("PS4 Pro", "Electronics",399.00 ,200 ),
+("PS3", "Electronics",299.00 ,200 ),
+("PS2", "Electronics",199.00 ,200 ),
+("Playstaion", "Electronics",99.00 ,200 ),
+("Game Cube", "Electronics",99.00 ,200 ),
+("Atari Classic", "Electronics",99.00 ,200 );
 
+SELECT * FROM bamazon.products;
 
 
